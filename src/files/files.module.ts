d24e3f6260442d6da44873e1f118697f240c9adb
User <maxandreev27@gmail.com>
2025-06-25 19:51:19 +0300

    Add file serving, output static file after upload

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { FilesController } from './files.controller';
 import { FilesService } from './files.service';
+import { path } from 'app-root-path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
+    imports: [
+        ServeStaticModule.forRoot({
+            rootPath: `${path}/uploads`,
+            serveRoot: '/uploads',
+        }),
+    ],
     controllers: [FilesController],
     providers: [FilesService],
 })
